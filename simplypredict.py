5ad6873e578387d1894238581a1a432501b39ef0
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-05-09 15:31:04 +0530

    added countdown screen
    but not working

--- simplypredict.py
+++ simplypredict.py
@@ -1,7 +1,7 @@
 import tensorflow.keras
 import numpy as np
 import cv2
-
+import time
 np.set_printoptions(suppress=True)
 model = tensorflow.keras.models.load_model("keras_model.h5")
 
@@ -10,7 +10,8 @@ s = ["images/0.jfif", "images/1.jfif", "images/2.png","images/3.jfif"]
 
 img = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
+start=time.time()
+end=time.time()
 while True:
     ret, frame = img.read()
     frame = cv2.flip(frame, 1)
@@ -24,11 +25,24 @@ while True:
     image_array = np.asarray(image)
     normalized = (image_array.astype(np.float32) / 127.0) - 1
     data[0] = normalized
-
-    pre = model.predict(data)
-    result = cv2.imread(s[np.argmax(pre[0])])
-
-    cv2.imshow("img", frame)
-    cv2.imshow("result", result)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+    end=time.time()
+    if(end-start<3):
+        cv2.putText(frame, "Deliver in", (160, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        if(end-start<1):
+            cv2.putText(frame, "3", (160, 60),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        elif(end-start<1):
+            cv2.putText(frame, "2", (160, 60),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        else:
+            cv2.putText(frame, "1", (160, 60),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    else:
+        pre = model.predict(data)
+        result = cv2.imread(s[np.argmax(pre[0])])
+        cv2.imshow("img", frame)
+        cv2.imshow("result", result)
+        start=end
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
