4530ed88b09b9efff467781e438ecc7747036700
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-05-10 21:04:24 +0530

    Update simplypredict.py

--- simplypredict.py
+++ simplypredict.py
@@ -12,7 +12,7 @@ img = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 start=time.time()
 end=time.time()
-
+check=0
 while True:
     end=time.time()
     ret, frame = img.read()
@@ -20,9 +20,11 @@ while True:
     if not ret:
         continue
 
-    frame = cv2.rectangle(frame, (70, 70), (340, 340), (0, 0, 255), 3)
+    frame = cv2.rectangle(frame, (360, 100), (610, 350), (0, 0, 255), 3)
     frame2 = frame[70:340, 70:340]
-    
+    check=5-end+start
+    cv2.putText(frame, "Deliver within {}".format(int(check)), (200, 30),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
     image = cv2.resize(frame2, (224, 224))
     image_array = np.asarray(image)
     normalized = (image_array.astype(np.float32) / 127.0) - 1
@@ -31,8 +33,6 @@ while True:
     cv2.imshow("img", frame)  
     pre = model.predict(data)
     result = cv2.imread(s[np.argmax(pre[0])])
-    cv2.putText(frame, "Deliver in every 4 seconds", (160, 30),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     if(end-start>4):
         start=end 
         cv2.imshow("result", result)
